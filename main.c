db71caf49e52607bcd06aea951438982d23b6379
zaxar55640 <90439895+zaxar55640@users.noreply.github.com>
2021-10-08 08:51:41 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -4,14 +4,16 @@
 int main() {
     int a;
     scanf("%o", &a);
-    printf("%x\n", a);
-    printf("%x\n", a);
-    printf("%x\n", a << 4);
-    printf("%x\n", a);
-    printf("%x\n", ~a);
+    printf("Hexademical of a = %x\n", a);
+
+    printf("Hexademical of a = %x\n", a);
+    printf("Hexademical of a 4 bites left = %x\n", a << 4);
+
+    printf("Hexademical of a = %x\n", a);
+    printf("Hexademical of a denial = %x\n", ~a);
     int b;
     scanf("%x", &b);
-    printf("%x\n", b & a);
+    printf("Operation & between a and b = %x\n", b & a);
 
     return 0;
 }
